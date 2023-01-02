185a944e690c27575d76b2376ca46d486c8f8b38
bncblnc <bncblnc@gmail.com>
2023-01-02 16:36:19 -0300

    responsive images and js to nav

---
+++ script.js
@@ -0,0 +1,15 @@
+const navAbout = document.getElementById("link-about");
+const navFeatures = document.getElementById("link-features");
+const navTours = document.getElementById("link-tours");
+const navStories = document.getElementById("link-stories");
+const navBook = document.getElementById("link-book");
+
+function closeNav() {
+  document.getElementById("navi-toggle").checked = false;
+}
+
+navAbout.addEventListener("click", closeNav);
+navFeatures.addEventListener("click", closeNav);
+navTours.addEventListener("click", closeNav);
+navStories.addEventListener("click", closeNav);
+navBook.addEventListener("click", closeNav);
